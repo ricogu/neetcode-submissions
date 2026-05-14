57a04c874a2e3b6a13aef314dd3fbbdd5245b8ee
Rico Gu <ricogu@gmail.com>
2026-05-14 23:07:58 +0200

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.go
@@ -0,0 +1,37 @@
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	ajlist := make([][]int, numCourses)
+	inDegrees := make([]int,numCourses)
+    for _, prerequisite := range prerequisites {
+		ajlist[prerequisite[1]] = append(ajlist[prerequisite[1]],prerequisite[0])
+		inDegrees[prerequisite[0]]++
+	}
+
+    var stack []int 
+	for course, inDegree := range inDegrees {
+		if inDegree == 0 {
+			stack = append(stack, course)
+		}
+
+	}
+
+	for len(stack) > 0{
+		current := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+        for _, next := range ajlist[current] {
+			inDegrees[next]--
+			if inDegrees[next] == 0 {
+				stack = append(stack, next)
+			}
+		}
+	}
+
+	for _, inDegree := range inDegrees {
+         if inDegree > 0 {
+			return false
+		 } 
+	}
+
+	return true
+    
+}
